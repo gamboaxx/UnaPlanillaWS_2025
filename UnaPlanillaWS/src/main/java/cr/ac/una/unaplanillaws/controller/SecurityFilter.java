68e06b46aad7eed57914d1edce2a883437ea63c2
gamboaxx <luis.gamboa.retana@est.una.ac.cr>
2025-09-08 21:16:27 -0600

    Add JWT support and security filter to project
    Added jjwt dependencies to pom.xml for JWT handling. Introduced JwTokenHelper utility for generating and validating JWT tokens. Created a basic SecurityFilter class as a provider for authentication, laying groundwork for securing REST endpoints.

---
+++ UnaPlanillaWS/src/main/java/cr/ac/una/unaplanillaws/controller/SecurityFilter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.unaplanillaws.controller;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author gambo
+ */
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class SecurityFilter {
+    
+}
---
+++ UnaPlanillaWS/src/main/java/cr/ac/una/unaplanillaws/util/JwTokenHelper.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.unaplanillaws.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author gambo
+ */
+public class JwTokenHelper {
+    private static JwTokenHelper jwTokenHelper = null;
+    private static final long EXPIRATION_LIMIT = 1;
+    private static final long EXPIRATION_RENEWAL_LIMIT = 5;
+    private static final String AUTHETICATION_SCHEME = "Bearer ";
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    
+    private JwTokenHelper() {
+    }
+
+    public static JwTokenHelper getInstance() {
+        if (jwTokenHelper == null) {
+            jwTokenHelper = new JwTokenHelper();
+        }
+        return jwTokenHelper;
+    }
+    
+    public String generatePrivateKey(String username){
+        return AUTHETICATION_SCHEME + Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(getExpirationDate())
+                .signWith(key)
+                .compact();
+    }
+    
+    private Date getExpirationDate(){
+        long currentTimeInMillis = System.currentTimeMillis();
+        long expMilliSecond = TimeUnit.MINUTES.toMillis(EXPIRATION_LIMIT);
+        
+        return new Date(currentTimeInMillis + expMilliSecond);
+    }
+    
+    public Claims claimKey(String privateKey){
+        return Jwts.parser().setSigningKey(key)
+                .parseClaimsJws(privateKey)
+                .getBody();
+    }
+    
+}
